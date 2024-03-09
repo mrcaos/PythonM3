05a743d7eab097da2f315adaa44a9fabd2ba64ee
J.aguilera <j.aguilera.duhalde@gmail.com>
2024-03-08 21:12:23 -0300

    agregar pip y django

---
+++ dia2/librerias.py
@@ -0,0 +1,16 @@
+#Las impotaciones se agregan en las primeras lineas de codigo
+import os
+import math 
+from math import sqrt
+
+#operdores 
+print(2 ** 4) # Exponenciación/Potencia. Ej: 2 elevado a 4 es 16
+print(5 // 2) # División Entera. Ej: 5 dividido 2 es 2.
+print(5 / 2) # División con decimales. Ej: 5 dividido 2 es 2.5
+print(5 % 2) # Módulo. Ej: 5 Módulo 2 es 1. 1 es el resto de 5 // 2.
+
+
+print(os.getcwd()) #
+print(math.sqrt(9))#
+#print(ceil(3.1416)) #importacion especifica de una libreria 
+print(math.ceil(3.1416))
