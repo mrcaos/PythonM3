e239199fcf4a5ca0e7136fc032524310facc0fb1
Juan Aguilera <j.aguilera.duhalde@gmail.com>
2024-03-10 11:34:25 +0000

    parte 1 presentador

---
+++ dia2/presentador.py
@@ -0,0 +1,15 @@
+"""
+Mi nombre es Bill, tengo 52 años y me desempeño como CEO en Microsoft.
+Soy Creativo, Apasionado y Visionario.
+Mi pasatiempo es jugar Golf y me gustaría poder jubilarme pronto
+"""
+
+nombre = input ("Bill")
+edad = input("52")
+ocupacion = input("CEO en Microsoft")
+lugar = input ("Microsoft")
+caracteristicas_1 = input ("Ingrese 3 caracteristicas:/n1 Creativo >")
+caracteristicas_2 = input ("2. Apasionado >")
+caracteristicas_2 = input ("3. Visionario >")
+pasatiempo = input ("Cual es tu pasatiempo: Jugar Golf ")
+hacer = input ("Me gustaría poder jubilarme pronto")
