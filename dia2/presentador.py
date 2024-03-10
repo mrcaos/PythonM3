9b2e774c5cb0c54366d27a8bef3dd9a12ed4814e
Juan Aguilera <j.aguilera.duhalde@gmail.com>
2024-03-10 12:23:33 +0000

    Bill ingresado

--- dia2/presentador.py
+++ dia2/presentador.py
@@ -4,12 +4,19 @@ Soy Creativo, Apasionado y Visionario.
 Mi pasatiempo es jugar Golf y me gustaría poder jubilarme pronto
 """
 
-nombre = input ("Bill")
-edad = input("52")
-ocupacion = input("CEO en Microsoft")
-lugar = input ("Microsoft")
-caracteristicas_1 = input ("Ingrese 3 caracteristicas:/n1 Creativo >")
-caracteristicas_2 = input ("2. Apasionado >")
-caracteristicas_2 = input ("3. Visionario >")
-pasatiempo = input ("Cual es tu pasatiempo: Jugar Golf ")
-hacer = input ("Me gustaría poder jubilarme pronto")
+nombre = input('Ingrese su Nombre: Bill')
+edad = input('Ingrese Edad: 52 años')
+ocupacion = input('Ingrese Ocupación: Ceo')
+lugar = input('En dónde?: Microsoft')
+caracteristica_1 = input('Ingrese 3 características:\n1. > Creativo')
+caracteristica_2 = input('2. > Apasionado')
+caracteristica_3 = input('3. > Visionario')
+pasatiempo = input('Cuál es tu pasatiempo: Jugar Golf')
+hacer = input('¿Que quieres hacer? Jubilarme pronto')
+
+print(f'''
+Mi nombre es {nombre}, tengo {edad} años y me desempeño como {ocupacion} en {lugar}.
+Soy una persona {caracteristica_1}, {caracteristica_2} y {caracteristica_3}.
+
+Mi pasatiempo es {pasatiempo} y me gustaría poder {hacer}.''')
+
