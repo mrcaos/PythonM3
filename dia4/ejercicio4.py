631f0d485847ea6b02b3328ca5dbc3ed7de67020
J.aguilera <j.aguilera.duhalde@gmail.com>
2024-03-12 22:07:48 -0300

    inicioFin

---
+++ dia4/ejercicio4.py
@@ -0,0 +1,49 @@
+"""
+Solicitar al usuario el ingreso de 3 numeros y determinar cuales
+de ellos es mayor que 33
+solo numeros enteros 1 al 100
+y determina cual es mayor y cual es menor
+"""
+#Verificar que los numeros estan dentro del rango
+
+si (numero1 < 1 o numero1 > 100 o numero2 < 1 o numero2 > o 100 numero3 < 1 o numero3 > 100 )
+    escribir ("Ingresar numeros enteros entre 1 y 100")
+    sino
+    
+    #Determinar cuales son mayores a 33
+    
+    si(numero1>33) entonces
+    escribir("El numero1 es mayor que 33.")
+    fin si
+    
+    si(numero2>33) entonces
+    escribir("El numero2 es mayor que 33.")
+    fin si
+    
+    si(numero3>33) entonces
+    escribir("El numero3 es mayor que 33.")
+    fin si
+    
+    #identificar el numero mayor y el numero menor
+    mayor = max(numero1,numero2,numero3)
+    menor = min(numero1,numero2,numero3)
+    
+    #mostrar los resultados
+    escribir("el numero mayor es:", mayor)
+    escribir("el numero menor es:", menor)
+
+
+
+"""
+hacer el calculo de tres promedios 
+
+"""
+
+Inicio
+    Nota1
+    Nota2
+    Nota3
+Calculo    
+    Promedio = (nota1 + nota2 + nota3) / 3 
+Mostrar promedio con dos decimales
+Fin
