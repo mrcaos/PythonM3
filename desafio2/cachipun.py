697b14e8fd75c1a79865ebd236ad52336ff3d49e
J.aguilera <j.aguilera.duhalde@gmail.com>
2024-03-15 20:01:28 -0300

    optimizacion

--- desafio2/cachipun.py
+++ desafio2/cachipun.py
@@ -30,3 +30,39 @@ elif jugador == "papel" and computador == "tijera":
     
 else:
     print("Argumento invalido: Debe ser piedra, papel o tijera.")
+    
+    
+"""
+OPTIMIZACION
+
+import sys
+import random
+
+opcion_jugador= sys.argv[1].lower()  #TIJERAS o tijeras o TiJerAs
+
+if(opcion_jugador != "piedra" and opcion_jugador != "papel" and opcion_jugador != "tijeras" ):
+    print("Argumento invalido: Debe ser piedra o papel o tijeras")
+else:#ingreso valido de los parametros
+    print("continua el programa")
+
+
+
+if(opcion_jugador == "piedra" or opcion_jugador == "papel" or opcion_jugador == "tijeras" ):
+    print("Datos ingresados correctamente!!")
+
+    print(f"la opcion de usuario es {opcion_jugador} ")
+
+    #ramdom - >elije al azar de una lista
+    opcion_maquina = random.choice(['piedra','papel','tijeras'])
+    print(f"la opcion del computador es: {opcion_maquina} ")
+
+    if opcion_jugador.lower() == opcion_maquina.lower():
+        print("Empate")
+    elif (opcion_jugador == "piedra" and opcion_maquina == "tijeras") or (opcion_jugador == "tijeras" and opcion_maquina == "papel") or (opcion_jugador == "papel" and opcion_maquina == "piedra"):    
+        print("Jugador gana")
+    else:
+        print("Perdiste!!")
+else:
+    print("Argumento invalido: Debe ser piedra o papel o tijeras")
+    
+"""
--- desafio2/imc.py
+++ desafio2/imc.py
@@ -15,15 +15,55 @@ indiceMasaCorporal = float(pesoKilogramo/alturaMetros**2)
 print("Su IMC es ", pesoKilogramo/alturaMetros**2)
 
 
-if indiceMasaCorporal<18.5:
+if indiceMasaCorporal < 18.5:
     print("La clasificacion OMS es Bajo Peso")
-elif 18.5<=indiceMasaCorporal and indiceMasaCorporal<25:
+elif indiceMasaCorporal >= 18.5 and indiceMasaCorporal < 25:
     print("La clasificacion OMS es Adecuado")
-elif 25>=indiceMasaCorporal and indiceMasaCorporal<30:
+elif indiceMasaCorporal >= 25 and indiceMasaCorporal < 30:
     print("La clasificacion OMS es Sobrepeso")
-elif 30>=indiceMasaCorporal and indiceMasaCorporal<35:
+elif indiceMasaCorporal >= 30 and indiceMasaCorporal < 35:
     print("La clasificacion OMS es Obesidad Grado I")
-elif 35>=indiceMasaCorporal and indiceMasaCorporal<40:
+elif indiceMasaCorporal >= 35 and indiceMasaCorporal < 40:
     print("La clasificacion OMS es Obesidad Grado II")
-elif 40>=indiceMasaCorporal:
+elif indiceMasaCorporal >= 40:
     print("La clasificacion OMS es Obesidad Grado III")
+    
+    
+    
+"""
+OPTIMIZACION
+
+import sys
+# se ejecuta en la terminal: python3 desafio2/imc.py 81 178
+print(sys.argv)     #['desafio2/imc.py', '81', '178']
+print(sys.argv[1])  #81
+print(sys.argv[2])  #178
+
+peso_kilogramos= float(sys.argv[1])
+estatura = float(sys.argv[2])/100
+
+print(type(peso_kilogramos))
+print(type(estatura))
+
+#calculo del imc
+#imc = peso_kilogramos/ (estatura * estatura)
+imc = peso_kilogramos/ (estatura ** 2 )
+print(f"Tu IMC es de {round(imc,2)}")
+#print(f"Tu IMC es de {imc:.2}")
+
+if imc < 18.5:
+    clasificacion = "Bajo Peso"
+elif imc >=18.5 and imc < 25:
+    clasificacion = "Adecuado"
+elif imc < 30:
+    clasificacion = "Sobrepeso"
+elif imc < 35:
+    clasificacion = "Obesidad grado I"
+elif imc < 40:
+    clasificacion = "Obesidad grado II"
+elif imc >=40:
+    clasificacion = "Obesidad grado III"
+
+print(f"Tu clasificación según la OMS es: {clasificacion}")
+
+"""
